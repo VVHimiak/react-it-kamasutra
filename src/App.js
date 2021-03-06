71f65f3783b63c848950520f67d3126ed2c9f1e6
Volodymyr Himiak <vv.himiak@gmail.com>
2021-03-06 21:11:39 +0200

    start it-kamasutra path

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Technologies from './technologies'
+import Header from './header';
+
+
+const App = () => {
+  return (
+  <div>
+      <Header />
+      <Technologies />
+      
+  </div>);  
+}
+
+
+
+
+
+
+export default App;
